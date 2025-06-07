11fa1b3773387843997f50c71898fa9548de75b5
SHIN <akiyachiumi@gmail.com>
2025-06-07 12:29:37 +0800

    chore: install prettier and clsx

--- apps/soul-chat-web/src/components/Layout.tsx
+++ apps/soul-chat-web/src/components/Layout.tsx
@@ -1,12 +1,12 @@
-import clsx from "clsx";
+import clsx from 'clsx';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main
       className={clsx(
-        "flex justify-center",
-        "h-screen w-screen items-center",
-        "bg-linear-to-b from-violet-500 to-fuchsia-500"
+        'flex justify-center',
+        'h-screen w-screen items-center',
+        'bg-linear-to-b from-violet-500 to-fuchsia-500',
       )}
     >
       {children}
